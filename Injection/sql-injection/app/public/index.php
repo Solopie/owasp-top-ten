deecbf9aa67656aa34296ff2801a37ef67fb895f
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-06-23 13:28:19 +1000

    Test docker on another device

---
+++ Injection/sql-injection/app/public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <h1 class="my-5">Welcome to my site!</h1>
+    <a href="login.php" class="btn btn-primary">Login</a>
+    <a href="register.php" class="btn btn-primary ml-3">Register</a>
+    </p>
+</body>
+</html>
